393618ff0be81dad56f5382bc6efde84331894b2
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2023-02-18 12:10:52 +0530

    Added basic ui to Home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home.js";
+import Footer from "./components/layout/Footer.js";
+import Header from "./components/layout/Header.js";
 
 function App() {
   return (
-    <div className="App">
-      
-    <h1>ecommerse</h1>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/books" element={<BookList />} /> */}
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Product from './Product'
+
+const product = {
+  name: "Blue Tshirt",
+  Images: [
+    {
+      url: "https://www.mydesignation.com/wp-content/uploads/2020/01/purple-plain-tshirt-mydesignation-unisex-image.jpg",
+    },
+  ],
+  price: "300",
+  _id: "nidhin"
+};
+
+function Home() {
+  return (
+    <div>
+      <h1>welcome to store</h1>
+      <h2>Featured Products</h2>
+      <Product product={product} />
+      <Product product={product} />
+      <Product product={product} />
+      <Product product={product} />
+      <Product product={product} />
+      <Product product={product} />
+    </div>
+  );
+}
+
+export default Home
